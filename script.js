377df54fc5a01c6aeeed157bd2db45ce4e26d2f4
Aldona Tomzik <a.tomzik@gmail.com>
2021-12-11 16:39:51 -0700

    Generate table header

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const addBookForm = document.getElementById("add-book-form");
+const resultDiv = document.getElementById("result");
+
+const bookCategories = {
+  fiction: "Beletrystyka",
+  nonFiction: "Literatura faktu",
+  science: "Literatura popularnonaukowa",
+};
 
 const saveToLocalStorage = (key, data) => {
   const dataArray = getDataFromLocalStrage(key);
@@ -18,5 +25,32 @@ const addBook = (event) => {
   saveToLocalStorage("MyBooks", bookDataObject);
   event.target.reset();
 };
+
+const generateTableHeader = () => {
+  const tHeader = document.createElement("tr");
+  const tableHeaders = [
+    "Tytuł książki",
+    "Autor Książki",
+    "Priorytet przeczytania",
+    "Kategoria",
+  ];
+  tableHeaders.forEach((heading) => {
+    const th = document.createElement("th");
+    th.innerText = heading;
+    tHeader.appendChild(th);
+  });
+  return tHeader;
+};
+
+const showBooksData = (books) => {
+  if (books.length > 0) {
+    const table = document.createElement("table");
+    table.appendChild(generateTableHeader());
+    resultDiv.appendChild(table);
+  }
+};
+
+const books = getDataFromLocalStrage("MyBooks");
+showBooksData(books);
 
 addBookForm.addEventListener("submit", addBook);
