94de2c7d6ac30675a7ebe611037d2bf28baaa1a2
Aldona Tomzik <a.tomzik@gmail.com>
2021-12-11 11:54:15 -0700

    Read and write data to local storage

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const addBookForm = document.getElementById("add-book-form");
 
+const saveToLocalStorage = (key, data) => {
+  const dataArray = getDataFromLocalStrage(key);
+  dataArray.push(data);
+  localStorage.setItem(key, JSON.stringify(dataArray));
+};
+const getDataFromLocalStrage = (key) => {
+  return localStorage.getItem(key) !== null
+    ? JSON.parse(localStorage.getItem(key))
+    : [];
+};
+
 const addBook = (event) => {
   event.preventDefault();
   const bookFormData = new FormData(event.target);
   const bookDataObject = Object.fromEntries(bookFormData.entries());
+  saveToLocalStorage("MyBooks", bookDataObject);
   event.target.reset();
 };
 
